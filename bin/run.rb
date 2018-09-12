c3de6bc32fcb54f5700e1b17d1681c9452c1c0b5
DeCode2018 <decode2018@gmail.com>
2018-09-12 15:41:08 -0400

    added test

--- bin/run.rb
+++ bin/run.rb
@@ -3,3 +3,16 @@ require_relative '../config/environment'
 
 
 puts "HELLO WORLD"
+
+
+
+def take_test
+  puts "What is your name?"
+  name = gets.chomp.capitilize
+  puts "Welcome #{name}, lets test your personality!!!"
+
+  
+end
+
+def
+greet
